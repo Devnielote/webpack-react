c306340aa72292aa88f3ab2a10ca86354c47e40e
Devnielote <danielfc@DESKTOP-TUGS55M.localdomain>
2021-06-23 13:21:17 -0500

    Dis -> Dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dis'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: './'
     },
     resolve: {
         extensions: ['.js','.jsx'],
