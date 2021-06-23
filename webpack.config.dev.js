ffaa7cb92afa954397e487841edc4e61b8bc7075
Devnielote <danielfc@DESKTOP-TUGS55M.localdomain>
2021-06-23 13:23:42 -0500

    dis -> dist en modo development

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dis'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
     resolve: {
